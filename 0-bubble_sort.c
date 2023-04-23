7c85694316d8052de188293e3a70da0673e18dfb
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-23 10:46:23 +0000

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an arrray
+ * @array: array to sort
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, flag;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		flag = 0;
+		for (j = 0; j < (size - 1 - i); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			return;
+	}
+}
