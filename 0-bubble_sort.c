4429d25cc4412ad9e22c427e90caefb2ac253da5
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-23 11:28:42 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,52 +1,32 @@
 #include "sort.h"
 
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
-
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
+ * bubble_sort - sorts array
+ * @array: to be sorted
+ * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	int temp, k;
+	size_t i, j;
 
 	if (array == NULL || size < 2)
 		return;
-
-	while (bubbly == false)
+	for (i = 0; i < (size - 1); i++)
 	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+		k = 0;
+		for (j = 0; j < (size - 1 - i); j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				swap_ints(array + i, array + i + 1);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
-				bubbly = false;
+				k = 1;
 			}
 		}
-		len--;
+		if (k == 0)
+			return;
 	}
 }
