97b78d70d686055982753f6b966fe70b8f544df4
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-23 14:17:18 +0000

    sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,7 +7,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp, k;
+	int k;
 	size_t i, j;
 
 	if (array == NULL || size < 2)
@@ -19,9 +19,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap_int(&array[j], &array[j + 1]);
 				print_array(array, size);
 				k = 1;
 			}
---
+++ swap_int.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * swap_int - sorts two by swap
+ * @a: first int
+ * @b: secon int
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
