d1c0fb9534389be0b1626ac08bc052659c31b5e1
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-24 12:07:31 +0000

    sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /**
- * swapme - swap the nodes themselves.
- * @current: pointer.
- * @current_old: pointer.
- * @list: doubly linked list
+ * swapper - swapping a linked list
+ * @holder: current location of linked list
+ * @list: double pointer to list
+ * Return: return the new pointer
  */
 listint_t *swapper(listint_t *holder, listint_t **list)
 {
@@ -23,52 +23,52 @@ listint_t *swapper(listint_t *holder, listint_t **list)
 		*list = holder;
 	return (holder);
 }
+
 /**
- * cocktail_sort_list - cocktail_sort_list
- *
- * @list: doubly linked list
+ * cocktail_sort_list - sort linked list using cocktail sort method
+ * @list: input linked list
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *check = *list;
-	int flag;
+	listint_t *current;
 
-	if (!list)
+	int flag = 1;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	if (!(*list))
-		return;
-	if (!(*list)->next)
-		return;
-	do {
+
+	current = (*list);
+
+	while (flag != 0)
+	{
 		flag = 0;
-		while (check->next)
+		while (current->next != NULL)
 		{
-			if (check->n > check->next->n)
-			{
-				check = swapper(check->next, list);
-				print_list(* list);
-				flag = 1;
-			}
-			else
-			{
-				check = check->next;
-			}
-		}
-		check = check->prev;
-		while (check->prev != NULL)
-		{
-			if (check->n < check->prev->n)
-			{
-				check = swapper(check, list);
-				print_list(* list);
-				flag = 1;
-			}
-			else
-			{
-				check = check->prev;
-			}
-		}
-		check = check->next;
-	} while (flag == 1);
-}
 
+			if (current->n > current->next->n)
+			{
+				current = swapper(current->next, list);
+				print_list(*list);
+				flag = 1;
+			}
+			current = current->next;
+		}
+		current = current->prev;
+
+		while (current->prev != NULL)
+		{
+
+			if (current->prev->n > current->n)
+			{
+				current = swapper(current, list);
+				print_list(*list);
+				flag = 1;
+			}
+			else if (current->prev != NULL)
+				current = current->prev;
+		}
+
+		current = current->next;
+
+	}
+}
